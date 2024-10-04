1b8ae658bb51021df0fef93b36f7158e81207f56
A706\1dam <manucarrera34@gmail.com>
2024-10-04 15:47:14 +0200

    Examen Tema1

---
+++ EXAMEN UD1/src/main/java/org/example/Examen.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Examen {
+    public void ejercicio1(){
+        Scanner entrada = new Scanner(System.in);
+        int num=0;
+        boolean comp = true;//Declaro las varialbles
+
+        while(comp){
+            try{
+                System.out.println("Introduce un número...");
+                num = entrada.nextInt(); //Pido que meta un numero
+                comp = false; //Condicion para romper el bucle
+            }catch(InputMismatchException err){ //Si mete algo que no es numero salta a esta parte
+                System.out.println("ERROR. Introduce un valor válido...");
+                entrada.nextLine();
+            }
+        }
+        if (num < 0){ //Si el numero es negativo lo convierte en positivo
+            num = num*(-1);
+        }
+        System.out.println("El valor absoluto es "+num); //Dice el valor absolute del numero introducido
+    }
+    public void ejercicio2(){
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Introduce tu nombre:"); //Pido el nombre del usuario
+        String nombre = entrada.next();
+        System.out.println("La obra de "+nombre);
+        System.out.println("Introduce el ancho de la pared..."); //Empiezo a pedir 4 numeros
+        float anchpar = entrada.nextInt();
+        System.out.println("Introduce el largo de la pared...");
+        float larpar = entrada.nextInt();
+        System.out.println("Introduce el ancho del azulejo...");
+        float anchazu = entrada.nextInt();
+        System.out.println("Introduce el largo del azulejo...");
+        float larazu = entrada.nextInt();
+        if (anchazu!=larazu){ //Si el ancho y largo del azulejo son iguales se corta el programa
+            if (anchazu < anchpar || larazu < larpar){ //Si el azulejo es mas grande que la pared se corta el programa
+                float resul = (anchpar*larpar)/(anchazu*larazu);
+                System.out.println("Se necesitan "+resul+" azulejos."); //Se muestran cuantos azulejos se necesitan para rellenar la pared
+            }else{
+                System.out.println("El azulejo no puede ser más grande que la pared");
+            }
+        }else{
+            System.out.println("El azulejo no puede ser cuadrado.");
+        }
+
+    }
+}
---
+++ EXAMEN UD1/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Examen ejercicio1 = new Examen();
+        ejercicio1.ejercicio1();
+
+        //Examen ejercicio2 = new Examen();
+        //ejercicio2.ejercicio2();
+    }
+}
